9331782707270d5805f001b2d44ba74e538c64ea
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-04-27 16:52:39 +0200

    fine lavoro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //dichiarqazione variabili
-let classe;
+let classe, finalPrice;
 const kmPrice = 0.21;
 
 
@@ -13,37 +13,24 @@ let userAge = parseInt (prompt("inserisci la tua età"));
 console.log("età passeggero = ", userAge);
 
 
-//definire classe di età del passeggero
-if (userAge < 18){
-    classe = "minorenne";
-} else if (userAge >= 65){
-    classe = "senior";
-}else {
-    classe = "maggiorenne";
-}
-
-console.log("classe di età = ", classe);
-
-
-
 //il prezzo del biglietto è di 0.21 € al km
 let basePrice = chilometri * kmPrice;
 console.log("prezzo base biglietto = €", basePrice,);
 
 
-//calcolo prezzi scontati
-if (classe = "minorenne") {
-    let minorPrice = basePrice * 0.8;
-    console.log("prezzo minor = €", minorPrice);
-} else if (classe = "senior"){
-    let seniorPrice = basePrice * 0.6;
-    console.log("prezzo senior = €", seniorPrice);
+//calcolare il prezzo totale del viaggio
+if (userAge < 18) {
+    finalPrice = basePrice * 0.8;
+    console.log("prezzo minor = €", finalPrice);
+} else if (userAge >= 65){
+    finalPrice = basePrice * 0.6;
+    console.log("prezzo senior = €", finalPrice);
 } else{
-    
+    finalPrice = basePrice;
+    console.log("prezzo normale = €", finalPrice);
 }
 
 
-//calcolare il prezzo totale del viaggio:
-
-
 //output del prezzo finale con massimo due decimali
+
+document.getElementById("prezzo_biglietto").innerHTML = ("€" + finalPrice);
