a9da6fb03542c1f74c669e8661352ed944d14ba2
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-04-27 15:09:01 +0200

    km da percorrere

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 //chiedere all’utente il numero di chilometri che vuole percorrere
+let chilometri = prompt("inserisci chilometri da percorrere");
+console.log("chilometri da percorrere = ", chilometri);
 
 //chiedere l’età del passeggero
 
+
 //il prezzo del biglietto è di 0.21 € al km
+
 
 //va applicato uno sconto del 20% per i minorenni
 
+
 //va applicato uno sconto del 40% per gli over 65.
 
+
 //calcolare il prezzo totale del viaggio:
+
 
 //output del prezzo finale con massimo due decimali
