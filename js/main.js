f294bb8e88ed832372eff1b0f4142acaf69d551d
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-04-27 15:04:05 +0200

    impostazione lavoro

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//chiedere all’utente il numero di chilometri che vuole percorrere
+
+//chiedere l’età del passeggero
+
+//il prezzo del biglietto è di 0.21 € al km
+
+//va applicato uno sconto del 20% per i minorenni
+
+//va applicato uno sconto del 40% per gli over 65.
+
+//calcolare il prezzo totale del viaggio:
+
+//output del prezzo finale con massimo due decimali
