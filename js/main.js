66457c9f9f94b2f7b38d16743d6c611de6cdfe09
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-04-27 15:47:11 +0200

    prezzo base biglietti

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@
 let classe;
 const kmPrice = 0.21;
 
+
 //chiedere all’utente il numero di chilometri che vuole percorrere
 let chilometri = parseInt (prompt("inserisci chilometri da percorrere"));
 console.log("chilometri da percorrere = ", chilometri);
 
+
 //chiedere l’età del passeggero
 let userAge = parseInt (prompt("inserisci la tua età"));
 console.log("età passeggero = ", userAge);
+
 
 //definire classe di età del passeggero
 if (userAge < 18){
@@ -21,8 +24,11 @@ if (userAge < 18){
 
 console.log("classe di età = ", classe);
 
-//il prezzo del biglietto è di 0.21 € al km
 
+
+//il prezzo del biglietto è di 0.21 € al km
+let basePrice = chilometri * kmPrice;
+console.log("prezzo base biglietto = €", basePrice,);
 
 //va applicato uno sconto del 20% per i minorenni
 
